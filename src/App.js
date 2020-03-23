287f01556727d537ee802d1b606b5807a5d3b80a
Simon <29654835+absolutelysimon@users.noreply.github.com>
2020-03-23 16:18:47 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import Graph from "./Graph";
+import "./App.css";
+import { Button, MenuItem, Select, TextField } from "@material-ui/core/";
+import { Autocomplete } from "@material-ui/lab/";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [thedata, setThedata] = useState(false);
+  const [countries, setCountries] = useState([]);
+  const [countryCount, setCountryCount] = useState(0);
+  const [allCountries, setAllCountries] = useState([]);
+  const [dataCategory, setDataCategory] = useState("DEATHS");
+  const [timeshift, setTimeshift] = useState(false);
+  const proxyUrl = "https://floating-headland-43054.herokuapp.com/",
+    targetUrl = "https://corona.lmao.ninja/historical";
+
+  if (thedata) {
+    // console.log("Res array: " + res_arr);
+    // debugger;
+    return (
+      <>
+        <Graph
+          thedata={thedata}
+          countries={countries}
+          countryCount={countryCount}
+          allCountries={allCountries}
+          dataCategory={dataCategory}
+          timeshift={timeshift}
+        />
+        <Button
+          onClick={() =>
+            setCountries(countries => {
+              // debugger;
+              return [
+                ...countries,
+                allCountries[Math.floor(Math.random() * allCountries.length)]
+                  .country
+              ];
+            })
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Random Country
+        </Button>
+        <Select onChange={evt => setDataCategory(evt.target.value)}>
+          <MenuItem value={"CASES"}>Total Cases</MenuItem>
+          <MenuItem value={"RECOVERED"}>Recovered</MenuItem>
+          <MenuItem value={"DEATHS"}>Deaths</MenuItem>
+          <MenuItem></MenuItem>
+        </Select>
+
+        <Autocomplete
+          options={allCountries}
+          getOptionLabel={option => option.country}
+          id="timeshift"
+          debug
+          onChange={(evt, newValue) => setTimeshift(newValue.country)}
+          renderInput={params => (
+            <TextField {...params} label="Timeshift Base" margin="normal" />
+          )}
+        />
+        <Autocomplete
+          options={allCountries}
+          getOptionLabel={option => option.country}
+          id="comparison"
+          debug
+          onChange={(evt, newValue) =>
+            setCountries([...countries, newValue.country])
+          }
+          renderInput={params => (
+            <TextField {...params} label="Compare against" margin="normal" />
+          )}
+        />
+      </>
+    );
+  } else {
+    fetch(proxyUrl + targetUrl)
+      .then(blob => blob.json())
+      .then(data => {
+        // console.table(data);
+        // console.log(data);
+        setThedata(data);
+        let c_idx = 0;
+        let temp_arr = [];
+        for (let p of data) {
+          if (!temp_arr.find(ele => ele.country === p.country)) {
+            temp_arr.push({ country: p.country, index: c_idx });
+          }
+          c_idx += 1;
+        }
+        setAllCountries(temp_arr);
+        // debugger;
+        return {};
+      })
+      .catch(e => {
+        console.log(e);
+        return e;
+      });
+    return <div>Waiting...</div>;
+  }
 }
 
 export default App;
+
+/*
+        <Autocomplete
+          multiple
+          id="tags-standard"
+          options={top100Films}
+          getOptionLabel={option => option.title}
+          defaultValue={[top100Films[13]]}
+          renderInput={params => (
+            <TextField
+              {...params}
+              variant="standard"
+              label="Multiple values"
+              placeholder="Favorites"
+            />
+          )}
+        />
+        */
---
+++ src/Graph.js
@@ -0,0 +1,178 @@
+import React, { useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from "recharts";
+
+// function findCountry(thedata, country) {
+//   return thedata.filter(ent => ent.country === country);
+// }
+
+export default function Graph({
+  thedata,
+  countries,
+  countryCount,
+  allCountries,
+  dataCategory,
+  timeshift = false
+}) {
+  let new_chart_data = initializeDates();
+  let y_max = 0;
+  let shifted_startdate = 0;
+
+  function initializeDates() {
+    let temp_arr = [];
+    for (const [key, value] of Object.entries(thedata[0].timeline.cases)) {
+      temp_arr.push({ date: new Date(key) });
+    }
+    return temp_arr;
+  }
+
+  function randomCountry() {
+    addCountry(allCountries[Math.floor(Math.random() * 100)].country);
+  }
+
+  function addCountry(country, shift = 0) {
+    console.log("Adding " + country + " with a shift of " + shift);
+    // debugger;
+    let idx = 0;
+
+    switch (dataCategory) {
+      case "CASES":
+        for (const [key, value] of Object.entries(
+          thedata.find(ele => ele.country === country).timeline.cases
+        )) {
+          if (parseInt(value) > y_max) {
+            y_max = value;
+          }
+          if (idx - shift > -1 && idx - shift < new_chart_data.length) {
+            new_chart_data[idx - shift][country] = value;
+          }
+          idx += 1;
+        }
+        break;
+      case "DEATHS":
+        for (const [key, value] of Object.entries(
+          thedata.find(ele => ele.country === country).timeline.deaths
+        )) {
+          if (parseInt(value) > y_max) {
+            y_max = value;
+          }
+          // debugger;
+
+          if (idx - shift > -1 && idx - shift < new_chart_data.length) {
+            new_chart_data[idx - shift][country] = value;
+          }
+          idx += 1;
+        }
+        break;
+      case "RECOVERED":
+        for (const [key, value] of Object.entries(
+          thedata.find(ele => ele.country === country).timeline.recovered
+        )) {
+          if (parseInt(value) > y_max) {
+            y_max = value;
+          }
+
+          if (idx - shift > -1 && idx - shift < new_chart_data.length) {
+            new_chart_data[idx - shift][country] = value;
+          }
+          idx += 1;
+        }
+        break;
+      default:
+        console.log("It's none");
+        break;
+    }
+  }
+  function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  function calculateShift(timeshift) {
+    // debugger;
+    let idx = 0;
+    for (const [key, value] of Object.entries(
+      thedata.find(ele => ele.country === timeshift).timeline.deaths
+    )) {
+      if (value > 10) {
+        return idx;
+      }
+      idx += 1;
+    }
+  }
+
+  function getCountriesData() {
+    // debugger;
+    if (timeshift) {
+      shifted_startdate = calculateShift(timeshift);
+      addCountry(timeshift);
+      for (let i of countries) {
+        addCountry(i, calculateShift(i) - shifted_startdate);
+      }
+    } else {
+      for (let i of countries) {
+        // debugger;
+        addCountry(i);
+      }
+    }
+    console.table(new_chart_data);
+  }
+
+  // debugger;
+  getCountriesData();
+  console.log(thedata);
+  return (
+    <>
+      <div style={{ width: "100%", height: 700 }}>
+        <ResponsiveContainer>
+          <LineChart
+            data={new_chart_data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" label="Date" />
+            {console.log("Setting max to " + y_max)}
+            <YAxis domain={[0, parseInt(y_max)]} />
+            <Tooltip />
+            <Legend />
+            {timeshift ? (
+              <Line
+                key={timeshift}
+                type="monotone"
+                dataKey={timeshift}
+                stroke={getRandomColor()}
+              />
+            ) : (
+              ""
+            )}
+            {countries.map(country => (
+              <Line
+                key={country}
+                type="monotone"
+                dataKey={country}
+                stroke={getRandomColor()}
+              />
+            ))}
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </>
+  );
+}
